d2562d1c1bc36b40bac0d33172cee30444e85070
Ryan Villanueva <ryanvill@gmail.com>
2016-04-14 12:43:17 -0400

    Messenger refactor

--- server/components/messages/messenger/index.js
+++ server/components/messages/messenger/index.js
@@ -1,76 +1,10 @@
 var Promise = require("bluebird");
-var request = require("request");
-var Messages = require("../../messages");
-var Format = require('./messenger.formatter');
 var Messenger = require('./messenger.service');
-
-function sendToMessenger(message){
-  return new Promise(function(resolve, reject){
-    console.log('sending...')
-    console.log(message)
-      var options = {
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {
-          access_token: process.env.FB_PAGE_TOKEN
-        },
-        json: true,
-        body: message
-      }
-      request.post(options, function(err, response, body){
-        if(err){
-          console.log(err)
-          reject(err)
-        } else {
-          console.log(body)
-          resolve(body);
-        }
-      })
-  })
-}
-
-function processEachMessage(messages){
-  console.log(messages)
-  messages.forEach(function(message, j){
-    Format.toStandard(message)
-      .then(formatted => {
-        Messages.receive(formatted);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  })
-}
-
-function concatMessages(entries){
-  return new Promise(function(resolve, reject){
-    var concatenated = [];
-    entries.forEach(function(entry, i){
-      var messages = entry.messaging;
-      messages.forEach(function(message, j){
-        concatenated.push(message);
-      })
-    })
-    resolve(concatenated);
-  })
-}
-
-function filterOutDeliveries(messages){
-  return new Promise(function(resolve, reject){
-    console.log(messages)
-    var filtered = [];
-    messages.forEach(function(message, i){
-      if(message.message){
-        filtered.push(message);
-      }
-    })
-    resolve(filtered);
-  })
-}
 
 export function send(message) {
   return new Promise(function(resolve, reject){
     Format.toMessenger(message)
-      .then(formatted => sendToMessenger(formatted))
+      .then(formatted => Messenger.sendToMessenger(formatted))
       .then(res => resolve(res))
       .catch(err => {
         console.log(err);
@@ -80,10 +14,9 @@ export function send(message) {
 }
 
 export function receive(data){
-  var entries = data.entry;
-  concatMessages(entries)
-    //.then(messages => filterOutDeliveries(messages))
+  Messenger.concatMessages(data)
+    //.then(messages => Messenger.filterOutDeliveries(messages))
     //.then(filtered => Messenger.checkUsersExist(filtered))
-    .then(messages => processEachMessage(messages))
+    .then(messages => Messenger.processEachMessage(messages))
     .catch(err => console.log(err))
 }
--- server/components/messages/messenger/messenger.service.js
+++ server/components/messages/messenger/messenger.service.js
@@ -1,5 +1,8 @@
 'use strict';
 import User from '../../../api/user/user.model';
+var request = require("request");
+var Messages = require("../../messages");
+var Format = require('./messenger.formatter');
 
 export function checkUsersExist(messages){
   return new Promise(function(resolve, reject){
@@ -72,5 +75,68 @@ export function createMessengerUser(messengerId){
       .catch(err => {
         reject(err);
       })
+  })
+}
+
+export function sendToMessenger(message){
+  return new Promise(function(resolve, reject){
+    console.log('Sending message:')
+    console.log(message)
+      var options = {
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+          access_token: process.env.FB_PAGE_TOKEN
+        },
+        json: true,
+        body: message
+      }
+      request.post(options, function(err, response, body){
+        if(err){
+          console.log(err)
+          reject(err)
+        } else {
+          console.log(body)
+          resolve(body);
+        }
+      })
+  })
+}
+
+export function concatMessages(obj){
+  return new Promise(function(resolve, reject){
+    var entries = obj.entry;
+    var concatenated = [];
+    entries.forEach(function(entry, i){
+      var messages = entry.messaging;
+      messages.forEach(function(message, j){
+        concatenated.push(message);
+      })
+    })
+    resolve(concatenated);
+  })
+}
+
+export function processEachMessage(messages){
+  messages.forEach(function(message, j){
+    Format.toStandard(message)
+      .then(formatted => {
+        Messages.receive(formatted);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  })
+}
+
+export function filterOutDeliveries(messages){
+  return new Promise(function(resolve, reject){
+    console.log(messages)
+    var filtered = [];
+    messages.forEach(function(message, i){
+      if(message.message){
+        filtered.push(message);
+      }
+    })
+    resolve(filtered);
   })
 }
